10aaf399fb71bdcb77100b1f7c28e1b831fea28d
MAINUL ISLAM <mainulislam6168@gmail.com>
2023-05-07 10:33:31 +0600

    case.js file added

---
+++ js/case.js
@@ -0,0 +1,34 @@
+// function-1 to increase or decrease phone or case number
+function updateCaseNumber(isIncrease){
+    const caseNumberField  = document.getElementById('case-number-field');
+   const caseNumberString = caseNumberField.value ;
+   const previousCaseNumber = parseInt(caseNumberString);
+  let newCaseNumber;
+  if(isIncrease === true){
+    newCaseNumber = previousCaseNumber + 1;
+  }
+  else{
+    newCaseNumber = previousCaseNumber -1;
+  }
+   caseNumberField.value = newCaseNumber
+   return newCaseNumber
+};
+// function-2 update case price
+function updateCasePrice(newCaseNumber){
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalField = document.getElementById('case-total');
+     caseTotalField.innerText = caseTotalPrice;
+}
+
+document.getElementById('case-btn-plus').addEventListener('click',function(){
+  const newCaseNumber = updateCaseNumber( true);
+  updateCasePrice(newCaseNumber);
+
+ 
+  
+})
+
+document.getElementById('case-btn-minus').addEventListener('click',function(){
+ const newCaseNumber = updateCaseNumber(false);
+ updateCasePrice(newCaseNumber);
+})
